b21470b05564e6d9706ba854299ba3588317a747
Mateus Souza <mateus_msouza@outlook.com>
2019-09-01 20:41:15 -0300

    Incluindo o react no projeto

--- database/database.js
+++ database/database.js
@@ -5,7 +5,6 @@ module.exports = () => {
     process.env.DATABASE || 'cardzinho',
     process.env.DATABASE_USER || 'mateus',
     process.env.DATABASE_PASSWORD || 1234,
-    process.env.DATABASE_URL || "127.0.0.1",
     {
       dialect: 'postgres',
     },
