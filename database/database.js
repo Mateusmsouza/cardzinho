ed15b1a3061365ef915928d9bf7ac1ed6247f68e
Mateus Souza <mateus_msouza@outlook.com>
2019-09-01 21:08:42 -0300

    build 4

--- database/database.js
+++ database/database.js
@@ -5,11 +5,10 @@ module.exports = () => {
     return new Sequelize(process.env.DATABASE_URL);
   }else{
     return new Sequelize(
-      process.env.DATABASE || 'cardzinho',
-      process.env.DATABASE_USER || 'mateus',
-      process.env.DATABASE_PASSWORD || 1234,
+      'cardzinho',
+      'mateus',
+      1234,
       {
-        host: process.env.DATABASE_URL || '127.0.0.1',
         dialect: 'postgres',
       }
     );
