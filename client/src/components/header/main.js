7e2d9e8426956fb63b1060d3b394b58a296bb406
Mateus Souza <mateus_msouza@outlook.com>
2019-09-01 22:45:19 -0300

    Iniciando react e corrigindo build

---
+++ client/src/components/header/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header id="Header">
+    <h1>Cardzinho</h1>
+    <button>Quero um Cardzinho!</button>
+  </header>
+);
+
+export default Header;
