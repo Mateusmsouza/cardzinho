35d838cd6b1d142ac11d1d667a60381440f2d54d
Mateus Souza <mateus_msouza@outlook.com>
2019-09-04 18:14:07 -0300

    criando variáveis de exceção no back end

--- routes/cardRouter.js
+++ routes/cardRouter.js
@@ -5,7 +5,8 @@ module.exports = (app) => {
 
   /*app.route("/")
           .get((req, res) => res.status(200).send('Volte em breve, amigo. Estamos em desenvolvimento. - Mateus Souza'));
-*/
+*/let _errorJson = "error:'unexpectedError'";
+  let _warningCoolDown = "warning:'cooldownNotComplete'";
   app.route("/cartao")
       .get( (req, res) => {   
         res.header("Access-Control-Allow-Origin", "*");
@@ -16,10 +17,7 @@ module.exports = (app) => {
                               res.status(200).json(allSolicitations);
                                 
                             }))
-                            .catch( (error) => {
-                              console.log(error);
-                              res.status(501).send()
-                            } )
+                            .catch(error => res.status(501).json(errorJson).send())
           
       })
 
@@ -36,9 +34,10 @@ module.exports = (app) => {
                       if (registers.length == 0){
                         Solicitation.save()
                                     .then( (solicitation) => res.status(200).json(solicitation).send() )
-                                    .catch(error => res.status(501).json(error).send())
-                      }else res.status(500).json("warning:'cooldownNotComplete'").send(); 
+                                    .catch(error => res.status(501).json(errorJson).send())
+                      }else res.status(500).json(warningCoolDown).send(); 
                     })
+                    .catch(error => res.status(501).json(errorJson).send())
           // save to database and return to view         
       })
 
@@ -49,8 +48,7 @@ module.exports = (app) => {
         
         Solicitation.exclude(req.body)
           .then( excluded =>  res.status(200).json(excluded).send())
-          .catch( error => console.log(error) );
-       
+          .catch(error => res.status(501).json(errorJson).send())
       })
   app.get('/', (req, res) => {
         res.header("Access-Control-Allow-Origin", "*");
